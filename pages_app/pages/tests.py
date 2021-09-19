011b9ad3eb5defdbb193b94cd50d5fbf124600fc
Sam Donnermeyer <donnermeyersam@gmail.com>
2021-09-19 11:31:07 -0600

    Update

--- pages_app/pages/tests.py
+++ pages_app/pages/tests.py
@@ -3,10 +3,12 @@ from django.test import SimpleTestCase
 
 # Create Simple Test
 class SimpleTests(SimpleTestCase):
+    ## Home Page ##
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
+    ## About Page ##
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
