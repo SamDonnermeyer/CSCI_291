bf29feb0e159e144de2db5a743ed0db1ef55a866
Sam Donnermeyer <donnermeyersam@gmail.com>
2021-09-29 14:40:42 -0600

    Updates & Template Creation

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 # blog/urls.py
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 # url pattern
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
 ]
