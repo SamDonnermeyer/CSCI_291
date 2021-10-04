805a6426b4e31bc3bf369dfb103a80d215541f4c
Sam Donnermeyer <donnermeyersam@gmail.com>
2021-10-03 23:04:47 -0600

    Added CRUD functionality

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,5 +1,6 @@
 # blog/models.py
 from django.db import models
+from django.urls import reverse
 
 # Post Class
 class Post(models.Model):
@@ -12,3 +13,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView, 
+    DeleteView,
+)
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogListView(ListView):
@@ -10,3 +16,19 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
