b3c09f78c7278620bb7181e8f81dad70f6d865c4
Sam Donnermeyer <donnermeyersam@gmail.com>
2021-10-26 22:36:43 -0600

    Chapter 13

--- news/config/settings.py
+++ news/config/settings.py
@@ -44,8 +44,11 @@ INSTALLED_APPS = [
     # Local #
     'accounts',
     'pages',
+    'articles',
 
 ]
+
+TIME_ZONE = 'America/New_York'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- news/config/urls.py
+++ news/config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('articles/', include('articles.urls')),
     path('', include('pages.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
